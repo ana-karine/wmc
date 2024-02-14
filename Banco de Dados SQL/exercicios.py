ccb5d7ad0eb31195a4975252d0b642f389e358dc
Ana Karine <anakarine.ti@gmail.com>
2024-02-14 16:08:55 -0300

    exercicio 2

--- Banco de Dados SQL/exercicios.py
+++ Banco de Dados SQL/exercicios.py
@@ -9,8 +9,27 @@ cursor = conexao.cursor()
 (inteiro), nome (texto), idade (inteiro) e curso (texto).
 '''
 
-cursor.execute('CREATE TABLE alunos(id INT, nome VARCHAR(100), idade INT, curso VARCHAR(100));')
+#cursor.execute('CREATE TABLE alunos(id INT, nome VARCHAR(100), idade INT, curso VARCHAR(100));')
 
+'''
+2. Insira pelo menos 5 registros de alunos na tabela que você criou no
+exercício anterior.
+'''
+
+# Lista de registros a serem inseridos
+registros = [
+    (1, 'Ana Moreira', 30, 'Engenharia'),
+    (2, 'Maria Silva', 20, 'Medicina'),
+    (3, 'Lucia Pereira', 18, 'Engenharia'),
+    (4, 'Carlos Silva', 45, 'Direiro'),
+    (5, 'Maria Rodrigues', 58, 'Psicologia')
+]
+
+query1 = '''
+INSERT INTO alunos(id, nome, idade, curso) VALUES(?, ?, ?, ?);
+'''
+
+cursor.executemany(query1, registros)
 
 conexao.commit()
 conexao.close
