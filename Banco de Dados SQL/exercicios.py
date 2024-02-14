fc580558c51ebec8e09f47ecadb53dd67f6a2ecc
Ana Karine <anakarine.ti@gmail.com>
2024-02-14 16:25:58 -0300

    exercicio 3

--- Banco de Dados SQL/exercicios.py
+++ Banco de Dados SQL/exercicios.py
@@ -29,7 +29,40 @@ query1 = '''
 INSERT INTO alunos(id, nome, idade, curso) VALUES(?, ?, ?, ?);
 '''
 
-cursor.executemany(query1, registros)
+#cursor.executemany(query1, registros)
+
+'''
+3. Consultas Básicas
+Escreva consultas SQL para realizar as seguintes tarefas:
+a) Selecionar todos os registros da tabela "alunos".
+b) Selecionar o nome e a idade dos alunos com mais de 20 anos.
+c) Selecionar os alunos do curso de "Engenharia" em ordem
+alfabética.
+d) Contar o número total de alunos na tabela
+'''  
+
+dados_a = cursor.execute('SELECT * FROM alunos;')
+print("\nSelecionar todos os registros da tabela 'alunos:'\n")
+for dado in dados_a:
+    print(dado)
+
+dados_b = cursor.execute('SELECT nome, idade FROM alunos WHERE idade > 20;')
+print("\nSelecionar o nome e a idade dos alunos com mais de 20 anos:\n")
+for dado in dados_b:
+    print(dado)
+
+dados_c = cursor.execute('SELECT * FROM alunos WHERE curso = "Engenharia" ORDER BY nome;')
+print("\nSelecionar os alunos do curso de 'Engenharia' em ordem alfabética:\n")
+for dado in dados_c:
+    print(dado)
+
+cursor.execute('SELECT COUNT(*) FROM alunos;')
+total_alunos = cursor.fetchone()[0]
+print("\nContar o número total de alunos na tabela:\n")
+print(total_alunos)
+
+
+
 
 conexao.commit()
 conexao.close
